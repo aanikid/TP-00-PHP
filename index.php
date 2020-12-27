ebc1f7aa5543edba3a8dca804c89bce1749daea6
fatima med <medkorfatima@gmail.com>
2020-12-27 13:29:37 +0100

    creation navbar

--- index.php
+++ index.php
@@ -22,6 +22,22 @@
 <!-- header -->
 <header>
    <!-- navbar -->
+   <nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light bg-light ">
+  <a class="navbar-brand" href="#">NewsUp</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto ">
+      <a class="nav-link active " href="#">Acceuil<span class="sr-only">(current)</span></a>
+      <a class="nav-link " href="#">Articles</a>
+      <a class="nav-link" href="#">Inscription</a>
+      <a class="nav-link" href="#">Login</a>
+      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+    </div>
+  </div>
+  
+</nav>
    <!-- end navbar -->
 
 
