d2f0113088344ef5b34db741b2fe35a606e7ef46
fatima med <medkorfatima@gmail.com>
2020-12-28 22:38:20 +0100

    ajout titre et paragraphe  pour chaque  articles du caroussel dans header.php

--- layouts/header.php
+++ layouts/header.php
@@ -26,7 +26,7 @@
   <header>
     <!-- navbar -->
     <nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light bg-light shadow">
-    <a class="navbar-brand" href="#"> <img src="./assets/img/newsup-logo.png" alt="logo"></a>
+    <a class="navbar-brand" href="#"> <img src="../assets/img/newsup-logo.png" alt="logo"></a>
       <!-- toggle button -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -48,7 +48,7 @@
 
     <!-- image header -->
     <figure>
-      <img src="./assets/img/newsup-banner.jpg" alt="image article sur le covid">
+      <img src="../assets/img/newsup-banner.jpg" alt="image article sur le covid">
     </figure>
     
     <!-- end image header -->
@@ -66,6 +66,8 @@
       <figure>
       <img src="../assets/img/newsup-article1.png" alt="">
       </figure>
+      <h2>fidèle à l'info plus que jamais</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel amet fuga aliquid id laborum cupiditate itaque vero rerum. In consequuntur officia, reprehenderit ad perferendis delectus? Nostrum ut dignissimos voluptatem magnam.</p>
         
       </div>
       
@@ -73,24 +75,32 @@
         <figure>
           <img src="../assets/img/newsup-article3.png" alt="">
         </figure>
+        <h2>fidèle à l'info plus que jamais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ex accusamus delectus aut corrupti doloribus ratione, esse laudantium quo facere non aperiam odio in deserunt cumque recusandae vitae temporibus asperiores!</p>
       </div>
       
       <div class="my-card">
         <figure>
           <img src="../assets/img/newsup-article1.png" alt="">
         </figure>
+        <h2>fidèle à l'info plus que jamais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis perferendis neque, hic, veritatis vero possimus qui iure error, quos modi accusantium eligendi nobis aliquam praesentium iste consequatur ipsa asperiores nam.</p>
       </div>
 
       <div class="my-card">
         <figure>
           <img src="../assets/img/newsup-article2.png" alt="">
         </figure>
+        <h2>fidèle à l'info plus que jamais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit reprehenderit ratione nisi dignissimos dolorem, laborum maxime alias. Laborum, incidunt. Velit dolores adipisci dolore odit asperiores aspernatur, eaque sed culpa? Incidunt!</p>
       </div>
-      
+
       <div class="my-card">
         <figure>
           <img src="../assets/img/newsup-article3.png" alt="">
         </figure>
+        <h2>fidèle à l'info plus que jamais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, atque. Ex, tempore! Labore tempora a hic ut quaerat sit minus dolore maxime, aut quisquam error fuga blanditiis explicabo voluptatem asperiores.</p>
       </div>
     </div>
     <!-- end carousel -->
