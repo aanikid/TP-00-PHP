1c684be04b92cfc93c462d228e3fe5e1c02d9800
aanikid <lotfiaanikid8@gmail.com>
2021-01-02 21:51:37 +0100

    affichage de la page dashboard seulement au admin

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,7 @@
 <!-- page de l'admin article -->
+<?php
+include_once "config/config.php";
+include_once "config/db_connect.php";
+
+include_once HEADER_PATH;
+//requête HTTP en methode GET = renvoie sur la page index
