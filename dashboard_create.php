28fdd3412098f4ae9feee8b2223c7a9d565e6224
Adrien Boudet <adrien.boudet1@gmail.com>
2021-01-03 15:47:53 +0100

    creation page dashboard create

---
+++ dashboard_create.php
@@ -0,0 +1,10 @@
+<?php include_once "config/config.php";?>
+<?php include_once "config/db_connect.php";?>
+<?php include_once DASHBOARD_HEADER;?>
+<!--===============================================================================================================================-->
+
+<h2>ICI LE MAIN DE LA PAGE DASHBOARD CREATE </h2>
+
+<!--===============================================================================================================================-->
+<?php include_once DASHBOARD_FOOTER;?> 
+<?php include_once FOOTER_PATH;?> 
