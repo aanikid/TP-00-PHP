79461c3dc90373ecd43d2ea5ed1a439c1d47e0f6
Adrien Boudet <adrien.boudet1@gmail.com>
2021-01-03 13:47:07 +0100

    modif articles

--- articles.php
+++ articles.php
@@ -12,22 +12,27 @@
         <div class="row justify-content-center border border-info rounded mt-3 inline-block">
             <!-- photo de l'article -->
             <figure class="col-4 p-0 m-0">
-            <a href="#"><img class="w-100 h-100" src="assets/img/newsup-article1.png" alt="chien tenant un journal"></a>
+                <a href="#"><img class="w-100 h-100" src="assets/img/newsup-article1.png" alt="chien tenant un journal"></a>
             </figure>
             <!-- titre et résumé de l'article -->
             <div class="col-8 p-3">
-            <h2>
-                <a href="#"> titre de l'article</a>
-            </h2>
-            <p class="h4">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam eaque repudiandae iste similique Lorem ipsum dolor sit,
-                amet consectetur adipisicing elit. Ullam eaque repudiandae iste similique
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur qui dicta cupiditate deserunt labore tenetur eaque neque,
-                maiores dolorem nulla hic necessitatibus saepe adipisci ab iste, a, assumenda quo laudantium? <a href="#"> Lire la suite ...</a>
-            </p>
-            <p class="text-right">
-            publié le xx/xx/xxxx 
-            </p>
+                <h2>
+                    <a href="#"> titre de l'article</a>
+                </h2>
+                <p class="h4">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam eaque repudiandae iste similique Lorem ipsum dolor sit,
+                    amet consectetur adipisicing elit. Ullam eaque repudiandae iste similique
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur qui dicta cupiditate deserunt labore tenetur eaque neque,
+                    maiores dolorem nulla hic necessitatibus saepe adipisci ab iste, a, assumenda quo laudantium? <a href="#"> Lire la suite ...</a>
+                </p>
+                <div class="d-flex justify-content-between">
+                    <p>
+                        publié par John Doe
+                    </p>
+                    <p>
+                        publié le xx/xx/xxxx  
+                    </p>
+                </div>
             </div>
         </div>
     
@@ -241,6 +246,9 @@
     
     </div>
 
+<!-- include du layout register -->
+    <?php include_once INDEX_COMPONENT_REGISTER ?>
+
 <!--et le footer contient toute la fin -->
     <!-- appel du layout footer -->
-    <?php include_once "layouts/footer.php" ?>
+    <?php include_once FOOTER_PATH ?>
